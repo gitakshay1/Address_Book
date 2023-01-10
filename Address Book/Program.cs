88fac592244756d8f2baebc9f21de32a50d3494a
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-10 23:21:48 +0530

    [Akshay]-Added program to create contact

---
+++ Address Book/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Address_Book
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Addresbook book = new Addresbook();
+            book.CreateContact();
+
+            while (true)
+            {
+                Console.WriteLine("1.Create Contact");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        book.CreateContact();
+                        break;
+                    
+                }
+
+            }
+        }
+    }
+}
