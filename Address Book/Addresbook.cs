c7d45e96aa78878921e965d0c2f30a92046298a1
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-13 16:53:24 +0530

    [Akshay] Added Program for edit contact by searching by name

--- Address Book/Addresbook.cs
+++ Address Book/Addresbook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Address_Book
 {
     public class Addresbook
     {
+        ArrayList arrayList = new ArrayList();
         public void CreateContact()
         {
             Contact Contact = new Contact();
@@ -27,7 +29,83 @@ namespace Address_Book
             Contact.Phone = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Enter MailId");
             Contact.Email = Console.ReadLine();
+            arrayList.Add(Contact);
+        }
+        public void DisplayContact()
+        {
+            Console.WriteLine("*****Contact****");
+            foreach (Contact records in arrayList)
+            {
+                Console.WriteLine("First Name -" + records.Firstname);
+                Console.WriteLine("Last Name -" + records.Lastname);
+                Console.WriteLine("Address -" + records.Address);
+                Console.WriteLine("City -" + records.City);
+                Console.WriteLine("State -" + records.State);
+                Console.WriteLine("Zip -" + records.Zip);
+                Console.WriteLine("Phone Number -" + records.Phone);
+                Console.WriteLine("Email -" + records.Email);
+            }
+        }
+        public void EditContact()
+        {
+            Console.WriteLine("To Edit contact enter contact firstname");
+            string name = Console.ReadLine();
+            foreach (Contact record in arrayList)
+            {
+                if (record.Firstname == name)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(name + " is exist");
+                    Console.ResetColor();
+                    Console.WriteLine("To edit details Enter " + "\n1.LastName\n2.Address\n3.City\n4.State\n5.Zip\n6.PhoneNumber\n7.Email");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter new lastname");
+                            string newLastname = Console.ReadLine();
+                            record.Lastname = newLastname;
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter New Address ");
+                            string newAddress = Console.ReadLine();
+                            record.Address = newAddress;
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter New City ");
+                            string newCity = Console.ReadLine();
+                            record.City = newCity;
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter New State ");
+                            string newState = Console.ReadLine();
+                            record.State = newState;
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter New Zip ");
+                            long newZip = Convert.ToInt64(Console.ReadLine());
+                            record.Zip = newZip;
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter New PhoneNumber ");
+                            long newPhone = Convert.ToInt64(Console.ReadLine());
+                            record.Phone = newPhone;
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter New Email ");
+                            string newEmail = Console.ReadLine();
+                            record.Email = newEmail;
+                            break;
 
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Name does not exist");
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
--- Address Book/Program.cs
+++ Address Book/Program.cs
@@ -8,12 +8,18 @@ namespace Address_Book
             Addresbook book = new Addresbook();
             while (true)
             {
-                Console.WriteLine("1.Create Contact");
+                Console.WriteLine("1.Create Contact\n2 Display Contact\n3 Edit Contact");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         book.CreateContact();
+                        break;
+                    case 2:
+                        book.DisplayContact(); 
+                        break;
+                    case 3:
+                        book.EditContact();
                         break;
 
                 }
